331c9f1ca5f717158e26699487e356568e9664d7
bufferzone-cloud <jubelmobz19@gmail.com>
2026-02-06 20:23:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // ===== FIREBASE CONFIGURATION =====
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBrVtSAOckpj8_fRA3-0kI7vAzOpXDUqxs",
-    authDomain: "zynapse-68181.firebaseapp.com",
-    databaseURL: "https://zynapse-68181-default-rtdb.firebaseio.com",
-    projectId: "zynapse-68181",
-    storageBucket: "zynapse-68181.firebasestorage.app",
-    messagingSenderId: "841353050519",
-    appId: "1:841353050519:web:3b16d95d8f4cd3b9506cd2",
-    measurementId: "G-4764XLL6WS"
+  apiKey: "AIzaSyBrVtSAOckpj8_fRA3-0kI7vAzOpXDUqxs",
+  authDomain: "zynapse-68181.firebaseapp.com",
+  databaseURL: "https://zynapse-68181-default-rtdb.firebaseio.com",
+  projectId: "zynapse-68181",
+  storageBucket: "zynapse-68181.firebasestorage.app",
+  messagingSenderId: "841353050519",
+  appId: "1:841353050519:web:3b16d95d8f4cd3b9506cd2",
+  measurementId: "G-4764XLL6WS"
 };
 
 // ===== CLOUDINARY CONFIGURATION =====
